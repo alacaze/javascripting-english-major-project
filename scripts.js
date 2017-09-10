63ed2bb257e6bfe8c8570cbc27b3ce8d0f6a1d5a
Adam La Caze <alacaze@mac.com>
2017-09-10 16:07:33 +1000

    Prologue

--- scripts.js
+++ scripts.js
@@ -63,27 +63,70 @@
 // popResult(test)
 
 
-let userString, upperCaseMinusE, upperCasedString;
-// First, we need a string from the user.
-userString = prompt("What do you want to UPPeRCASe?");
-// Second, we need to create our function.
-upperCaseMinusE = function(string){
-	let result;
-	result = "";
-	for (let i = 0; i < string.length ; i = i + 1) {
-		let letter;
-		letter = string[i];
-		if ( letter === "e") {
-			result = result + letter;
-		} else {
-			result = result + letter.toUpperCase();
-		}
-	}
-	return result;
-};
-// Third, we need to pass the user’s string to the 
-// function and assign the return value to a 
-// variable.
-upperCasedString = upperCaseMinusE(userString);
-// And we can then print the string to the webpage.
-$("#response").html(upperCasedString);
+// let userString, upperCaseMinusE, upperCasedString;
+// // First, we need a string from the user.
+// userString = prompt("What do you want to UPPeRCASe?");
+// // Second, we need to create our function.
+// upperCaseMinusE = function(string){
+// 	let result;
+// 	result = "";
+// 	for (let i = 0; i < string.length ; i = i + 1) {
+// 		let letter;
+// 		letter = string[i];
+// 		if ( letter === "e") {
+// 			result = result + letter;
+// 		} else {
+// 			result = result + letter.toUpperCase();
+// 		}
+// 	}
+// 	return result;
+// };
+// // Third, we need to pass the user’s string to the 
+// // function and assign the return value to a 
+// // variable.
+// upperCasedString = upperCaseMinusE(userString);
+// // And we can then print the string to the webpage.
+// $("#response").html(upperCasedString);
+
+
+// let leonardo, donatello, raphael, michelangelo, turtle, weapons;
+// leonardo = {name: "Leonardo", color: "blue", weapon: "katana"};
+// donatello = {name: "Donatello", color: "purple", weapon: "bo"};
+// raphael = {name: "Raphael", color: "red", weapon: "sai"};
+// michelangelo = {name: "Michelangelo", color: "blue", weapon: "nunchaku"};
+// turtles = [leonardo, donatello, raphael, michelangelo, splinter];
+// weapons = turtles.map(function(turtle){
+// 	return turtle.weapon;
+// }).sort().join(", ");
+// let names, namesWithO;
+// names = turtles.map(function(turtle){
+//   return turtle.name;
+// }).sort();
+// namesWithO = names.filter(function(name){
+//   return name.includes("o");
+// }).join(", ");
+// $("#response").html(namesWithO);
+// $("#response").html(weapons);
+
+
+// let integers, double;
+// integers = [1, 2, 3];
+// double = integers.map(function(ints){
+// 	return integers * 2
+// });
+// $("#response").html(double);
+let turtlesWithSplinter, turtlesWithoutSplinter, reversedTurtlesWithoutSplinter;
+turtlesWithSplinter = ["Leonardo", "Donatello", "Raphael", "Michelangelo", "Splinter"];
+// oops. let's pop() Splinter off before reversing…
+// turtlesWithoutSplinter = turtlesWithSplinter.pop();
+// console.log(turtlesWithoutSplinter);
+//--> Splinter
+//reversedTurtlesWithoutSplinter = turtlesWithSplinter.pop().reverse();
+//$("#response").html(reversedTurtlesWithoutSplinter);
+// Use .filter() instead of .pop().
+reversedTurtlesWithoutSplinter = turtlesWithSplinter.filter(function(turtle){
+  // What is the value of turtle?
+  console.log(turtle);
+  return turtle !== "splinter";
+}).reverse();
+$("#response").html(reversedTurtlesWithoutSplinter);
